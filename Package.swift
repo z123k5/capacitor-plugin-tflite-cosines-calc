4cfa28e022b64b338fb2b40a5c8108d9643ebdc2
z123k5 <snownightabc@gmail.com>
2025-03-03 18:31:12 +0800

    bugfix

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "7.0.0")
-        .package(url: "https://github.com/tensorflow/tensorflow", .upToNextMajor(from: "2.12.0"))
+        .package(url: "https://github.com/apple/swift-tensorflow.git", .upToNextMajor(from: "0.12.0"))
     ],
     targets: [
         .target(
@@ -19,6 +19,7 @@ let package = Package(
             dependencies: [
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
                 .product(name: "Cordova", package: "capacitor-swift-pm")
+                .product(name: "TensorFlowLite", package: "swift-tensorflow")
             ],
             path: "ios/Sources/GalleryEnginePlugin"),
         .testTarget(
